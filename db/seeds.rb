42537da436cf791749d966191ec4e23dfbd832d0
Saranya Ganapathy <saranyaganapathy95@gmail.com>
2017-06-25 21:56:47 -0400

    Checkpoint 6 complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,22 +1,45 @@
-# #Create Users
-#
-# 5.times do
-#   user = User.create!(
-#   email:    Faker::Internet.email,
-#   password: Faker::Pokemon.name
-#   )
-# end
-# users = User.all
-#
-# #create wikis
-# 50.times do
-#   wiki.create!(
-#   title: Faker::Hipster.sentence
-#   body: Faker::Hipster.paragraph
-#   )
-# end
-# wikis = Wiki.all
-#
-# puts "Seed finished"
-# puts "#{User.count} users created."
-# puts "#{Wiki.count} posts created."
+5.times do
+  User.create!(
+  email: Faker::Internet.email,
+  password: Faker::Internet.password,
+  standard: true,
+  premium: false,
+  admin: false
+  )
+end
+
+3.times do
+  User.create!(
+  email: Faker::Internet.email,
+  password: Faker::Internet.password,
+  standard: false,
+  premium: true,
+  admin: false
+  )
+end
+
+2.times do
+  User.create!(
+  email: Faker::Internet.email,
+  password: Faker::Internet.password,
+  standard: false,
+  premium: false,
+  admin: true
+  )
+end
+
+users = User.all
+
+100.times do
+  Wiki.create!(
+  title: Faker::Hipster.sentence,
+  body: Faker::Hipster.paragraph,
+  private: false
+  )
+end
+
+wikis = Wiki.all
+
+puts "Seed finished"
+puts "#{users.count} users created"
+puts "#{wikis.count} wikis created"
